33fffdcf0d686679c0b865a14e00fa39507ed66a
Rijat <Rijat@DESKTOP-D454U9F>
2021-10-30 14:56:10 +0200

    Add project files.

---
+++ EskimoOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EskimoOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
